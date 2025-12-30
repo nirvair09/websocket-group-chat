d83658478373bbd2b2a695fd0c0582c81e21bbb6
nirvair09 <chandradeep262003@gmail.com>
2025-12-30 12:32:59 +0530

    feat: Add initial Socket.IO server for real-time group chat functionality.

--- server/server.js
+++ server/server.js
@@ -15,16 +15,27 @@ app.get('/', (req, res) => {
     res.send("hello")
 });
 
+const roomName = "room1";
 
 io.on("connection", (socket) => {
     console.log("a user connected", socket.id);
     console.log("socket info", socket);
 
-    socket.on("joinRoom", async (userName) => {
+    socket.on('joinRoom', async (userName) => {
         socket.to(roomName).emit("newUserJoined", userName)
     });
 
+    socket.on('chatMessage', async (message) => {
+        socket.to(roomName).emit('chatMessage', message);
+    });
 
+    socket.on('typing', async (userName) => {
+        socket.to(roomName).emit('typing', userName);
+    });
+
+    socket.on('stopTyping', async (userName) => {
+        socket.to(roomName).emit('stopTyping', userName);
+    });
 });
 
 io.on("disconnect", (socket) => {
